6e38e8c26c835b0d71e91c22c9f7219ab6a8fda9
Danish Mehmood <danishm7012@gmail.com>
2021-07-09 17:47:40 +0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b6a450c0f00b8308fbafc755e44abf2e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '51a0a1c3-3d15-490a-a81f-210424a64b12'
+};
